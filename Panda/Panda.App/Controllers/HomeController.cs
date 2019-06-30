4f7eb83a37e023226f015fa0354ae0e14739dc13
prohause@abv.bg <Sevdjan8092>
2019-06-30 07:32:34 +0300

    Index added

---
+++ Panda/Panda.App/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Panda.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
